b6ead166c78cef644c603707f922326c0b6eba5b
Vanessa Ribeiro <91354448+nessribeiro@users.noreply.github.com>
2025-09-21 14:42:18 -0300

    Update CartasSuperTrunfo.c
    Criação de variaveis estado e cidades

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,18 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char estado1 = A, estado2 = B, estado3 = C, estado4 = D, estado5 = E, estado6 = F, estado7 = G, estado8 = H;
+  //Variáveis para armezanar o nome das cidades
+  char nomeCidadeA01[20], nomeCidadeA02[20], nomeCidadeA03[20], nomeCidadeA04[20];
+  char nomeCidadeB01[20], nomeCidadeB02[20], nomeCidadeB03[20], nomeCidadeB04[20];
+  char nomeCidadeC01[20], nomeCidadeC02[20], nomeCidadeC03[20], nomeCidadeC04[20];
+  char nomeCidadeD01[20], nomeCidadeD02[20], nomeCidadeD03[20], nomeCidadeD04[20];
+  char nomeCidadeE01[20], nomeCidadeE02[20], nomeCidadeE03[20], nomeCidadeE04[20];
+  char nomeCidadeF01[20], nomeCidadeF02[20], nomeCidadeF03[20], nomeCidadeF04[20];
+  char nomeCidadeG01[20], nomeCidadeG02[20], nomeCidadeG03[20], nomeCidadeG04[20];
+  char nomeCidadeH01[20], nomeCidadeH02[20], nomeCidadeH03[20], nomeCidadeH04[20];
+  
+  
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
