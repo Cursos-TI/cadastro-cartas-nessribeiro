b583db3599127be0f43aa21e108bec367a1a3d38
Vanessa Ribeiro <91354448+nessribeiro@users.noreply.github.com>
2025-09-28 16:55:57 -0300

    Update CartasSuperTrunfo.c
    Alteração de código para apenas 02 cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,41 +6,84 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  char estado1 = A, estado2 = B, estado3 = C, estado4 = D, estado5 = E, estado6 = F, estado7 = G, estado8 = H;
-  //Variáveis para armezanar o nome das cidades
-  char nomeCidadeA01[20], nomeCidadeA02[20], nomeCidadeA03[20], nomeCidadeA04[20];
-  char nomeCidadeB01[20], nomeCidadeB02[20], nomeCidadeB03[20], nomeCidadeB04[20];
-  char nomeCidadeC01[20], nomeCidadeC02[20], nomeCidadeC03[20], nomeCidadeC04[20];
-  char nomeCidadeD01[20], nomeCidadeD02[20], nomeCidadeD03[20], nomeCidadeD04[20];
-  char nomeCidadeE01[20], nomeCidadeE02[20], nomeCidadeE03[20], nomeCidadeE04[20];
-  char nomeCidadeF01[20], nomeCidadeF02[20], nomeCidadeF03[20], nomeCidadeF04[20];
-  char nomeCidadeG01[20], nomeCidadeG02[20], nomeCidadeG03[20], nomeCidadeG04[20];
-  char nomeCidadeH01[20], nomeCidadeH02[20], nomeCidadeH03[20], nomeCidadeH04[20];
-
-  //Variáveis das propriedades - ESTADO A
-  int populacaoA01, populacaoA02,populacaoA03, populacaoA04;
-  float areaA01, areaA02, areaA03, areaA04;
-  float pibA01, pibA02, pibA03, pibA04;
-  int pontosTuristicosA01, pontosTuristicosA02, pontosTuristicosA03, pontosTuristicosA04;
-
-  //Variáveis das propriedades - ESTADO B
-  int populacaoB01, populacaoBA02,populacaoB03, populacaoB04;
-  float areaB01, areaB02, areaB03, areaB04;
-  float pibB01, pibB02, pibB03, pibB04;
-  int pontosTuristicosA01, pontosTuristicosA02, pontosTuristicosA03, pontosTuristicosA04;
-  
-  //Variáveis das propriedades - ESTADO C
-  //Variáveis das propriedades - ESTADO D
-  //Variáveis das propriedades - ESTADO E
-  //Variáveis das propriedades - ESTADO F
-  //Variáveis das propriedades - ESTADO G
-  //Variáveis das propriedades - ESTADO H
-  
-  
+    char Estado1[20], Estado2[20];
+    char codigoCarta1[3], codigoCarta2[3];
+    char nomeCidade1[20], nomeCidade2[20];
+    int populacao1, populacao2;
+    float area1, area2;
+    float PIB1, PIB2;
+    int pontosTuristicos1, pontosTuristicos2;
   
   // Área para entrada de dados
+    printf("Olá! Seja bem vinda(o) ao Super Trunfo - Cidades \n");
+    printf("Para iniciar o jogo precisaremos das informações das cidades escolhidas. \n");
+    printf("Nas próximas etapas insira as informações da 1ª carta: \n");
 
+    //Recebendo as informações da primeira carta
+    printf("Digite a categoria do Estado (A a H): \n");
+    scanf("%s", &Estado1);
+    
+    printf("Digite o código da carta: (primeira letra + dois digitos de 01 a 04)\n");
+    scanf("%s", &codigoCarta1);
+   
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &nomeCidade1);
+
+    printf("Insira o número populacional: \n");
+    scanf("%i", &populacao1);
+
+    printf("Insira a área desta cidade em km: \n");
+    scanf("%f", &area1);
+    
+    printf("Insira o PIB: \n");
+    scanf("%f", &PIB1);
+
+    printf("Insira a quantidade de pontos turisticos que essa cidade tem: \n");
+    scanf("%i", &pontosTuristicos1);
+
+    //Recebendo as informações da segunda carta
+    printf("Agora insira os dados da 2ª Carta \n");
+
+    printf("Digite o nome do Estado: \n");
+    scanf("%s", &Estado2);
+    
+    printf("Digite o código da carta: (primeira letra + dois digitos de 01 a 04)\n");
+    scanf("%s", &codigoCarta2);
+   
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &nomeCidade2);
+
+    printf("Insira o número populacional: \n");
+    scanf("%i", &populacao2);
+
+    printf("Insira a área desta cidade em km: \n");
+    scanf("%f", &area2);
+    
+    printf("Insira o PIB: \n");
+    scanf("%f", &PIB2);
+
+    printf("Insira a quantidade de pontos turisticos que essa cidade tem: \n");
+    scanf("%i", &pontosTuristicos2);
+  
   // Área para exibição dos dados da cidade
-
+    printf("As cartas inseridas foram: \n");
+    //impressão da carta 1
+    printf("Carta 1: \n");
+    printf("Nome da Cidade: %s \n", nomeCidade1);
+    printf("Estado: %s \n", Estado1);
+    printf("Código: %s \n", codigoCarta1);
+    printf("População: %i \n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", PIB1);
+    printf("Número de pontos turisticos: %i \n", pontosTuristicos1);
+    //impressão da carta 1
+    printf("Carta 2: \n");
+    printf("Nome da Cidade: %s \n", nomeCidade2);
+    printf("Estado: %s \n", Estado2);
+    printf("Código: %s \n", codigoCarta2);
+    printf("População: %i \n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", PIB2);
+    printf("Número de pontos turisticos: %i \n", pontosTuristicos2);
 return 0;
 } 
